48c9adb3cf971493ee7905d312a7af49ccf02370
Peter Blajev <pblajev@gmail.com>
2021-12-08 23:54:35 -0800

    Find the answer.

--- day3a.py
+++ day3a.py
@@ -1,69 +1,79 @@
 #!/usr/bin/env python
 
-# Build a string from each column and then use
-# Counter(string) and it will count the 1s and 0s
-
 from collections import Counter
 
 def main():
     # Define the dictionary we are going to use
     line_d = {}
+    workLine_d = {}
     newLine_d = {}
     lineN = 1
     posN = 0
-    workLine = ''
     buildLine = ''
 
-    f = open('day3-input-short.txt', 'r')
-    #f = open('day3-input.txt', 'r')
+    #f = open('day3-input-short.txt', 'r')
+    f = open('day3-input.txt', 'r')
 
     # Build the line dictionary
     for line in f:
         line_d[lineN] = line
         lineN += 1
 
-    print(line_d)
-
     # Cycle through every position
-    while posN < len(line)-1:
-        # Build the string for column in posN
-        for key in line_d:
-            workLine = line_d[key]
-            buildLine = buildLine+workLine[posN]
+    for mostleast in ['most', 'least']:
+        print()
+        print('==> Finding ', mostleast)
+        workLine_d = line_d
+        print(workLine_d)
 
-        print(buildLine)
+        while posN < len(line)-1:
+            # Build the string for column in posN
+            for key in workLine_d:
+                workLine = workLine_d[key]
+                buildLine = buildLine+workLine[posN]
 
-        # Count the digits in the string to find out the most common
-        element = Counter(buildLine)
-        if element['1'] < element['0']:
-            toKeep = 0
-        else:
-            toKeep = 1
+            print(buildLine)
 
-        print('We are keeping: ', toKeep, 'at position: ', posN)
+            # Count the digits in the string to find out the most common
+            element = Counter(buildLine)
+            if mostleast == 'most':
+                if element['1'] < element['0']:
+                    toKeep = 0
+                else:
+                    toKeep = 1
+            else:
+                if element['1'] < element['0']:
+                    toKeep = 1
+                else:
+                    toKeep = 0
 
-        # Build the new dictionary
-        for key in line_d:
-            if line_d[key][posN] == str(toKeep):
-                newLine_d[key] = line_d[key]
+            print('We are keeping: ', toKeep, 'at position: ', posN)
 
-        line_d = newLine_d
-        print(line_d)
+            # Build the new dictionary
+            for key in workLine_d:
+                if workLine_d[key][posN] == str(toKeep):
+                    newLine_d[key] = workLine_d[key]
 
-        if len(line_d.keys()) == 1: break
+            workLine_d = newLine_d
+            print(workLine_d)
 
-        posN += 1
-        buildLine = ''
-        newLine_d = {}
+            posN += 1
+            buildLine = ''
+            newLine_d = {}
 
-#    # Calculate gama and epsilon
-#    gamaDecimal = int(gama, 2)
-#    epsilonDecimal = int(epsilon, 2)
-#
-#    print(gamaDecimal)
-#    print(epsilonDecimal)
-#
-#    print(gamaDecimal*epsilonDecimal)
+            # Break out of the while loop if we are at the last element
+            if len(workLine_d.keys()) == 1:
+                if mostleast == 'most':
+                    for k in workLine_d: oxygen = workLine_d[k]
+                if mostleast == 'least':
+                    for k in workLine_d: co2 = workLine_d[k]
+                posN = 0
+                break
+
+    print('I got oxygen: ', oxygen)
+    print('I got    co2: ', co2)
+
+    print(int(oxygen, 2)*int(co2, 2))
 
     f.close()
 
